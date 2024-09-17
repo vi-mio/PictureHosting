9b35ad5b6b762b4b5677b174adf206a657cf8afe
Vimio <115444714+vi-mio@users.noreply.github.com>
2024-09-17 23:44:50 +0800

    Add files via upload

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,7 +1,5 @@
 <template>
 	<div class="dashboard-container">
-<!--		<div class="dashboard-text">汽院桃四905宿舍-2021~2025学年的大帅哥们的专属图床网站</div>-->
-<div><img src="@/assets/help_images/guohui.jpg"></div>
     <div><h2  style="color: red">使用手册:</h2>
       <h3>本站仅供个人学习使用，禁止用于商业用途，因传播者的原因造成法律纠纷的，由传播者自行负责。</h3>
       <h3>本站上传的图片使用的是国内的CND加速服务，禁止上传(露骨/血腥/暴力/吸毒/R18)违法图片，否则将永久封禁，请遵守中华人民共和国相关法律法规，做一个爱国爱党，遵纪守法的中国公民。</h3>
